6204aadafb9fc945e7f8221027dbad59a298a8b7
Imon Roy <royimonroy@gmail.com>
2022-07-25 18:40:24 +0530

    added admin

--- test_app/admin.py
+++ test_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Board)
+admin.site.register(Standard)
+admin.site.register(Subject)
+admin.site.register(Topic)
+admin.site.register(Question)
+admin.site.register(Test)
+admin.site.register(TestQuestionMapping)
