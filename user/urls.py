ac16fad2a6b89c02f7660a71ed322fda15cb4fed
Imon Roy <royimonroy@gmail.com>
2022-08-06 20:28:32 +0530

    updated everything cause gotta bring home the bucks

---
+++ user/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import UserCreateView
+
+urlpatterns = [
+    path("", UserCreateView.as_view(), name="user-create"),
+]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import generics,permissions
+from rest_framework.response import Response
+from .serializers import UserSerializer
 
-# Create your views here.
+class UserCreateView(generics.CreateAPIView):
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = UserSerializer
+    
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        if not user.is_authenticated:
+            return Response({
+                "message":"Please Login"
+            })
+        serializer = UserSerializer(user)
+        return Response(serializer.data,status=200)
+
